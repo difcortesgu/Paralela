a9c46a36483dfd36904f099ac947a63ba28f8916
Farid <difcortesgu@unal.edu.co>
2021-04-18 19:00:38 -0500

    Version secuencial lista

--- Practicas/1/test.cpp
+++ Practicas/1/test.cpp
@@ -2,6 +2,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <vector>
+
 
 using namespace cv;
 using namespace std;
@@ -15,29 +17,60 @@ int main(int argc, char **argv)
     //     return -1;
     // }
     
-    // int kernel[3][3] = {
+    // DETECCION DE BORDES
+    // vector<vector<int>> kernel = {
     //     {0, 1, 0},
     //     {1, -4, 1},
     //     {0, 1, 0}
     // };
 
-    // int kernel[3][3] = {
-    //     {0, 1, 0},
-    //     {1, 5, 1},
-    //     {0, 1, 0}
+    // REPUJADO
+    // vector<vector<int>> kernel = {
+    //     {-2, -1, 0},
+    //     {-1, 1, 1},
+    //     {0, 1, 2}
     // };
-    
-    // int kernel[3][3] = {
+
+    // ENFOCADO
+    // vector<vector<int>> kernel = {
+    //     {0, -1, 0},
+    //     {-1, 5, -1},
+    //     {0, -1, 0}
+    // };
+
+    // ENFOCADO
+    // vector<vector<int>> kernel = {
+    //     {0, 0, 0, 0, 0},
+    //     {0, 0, -1, 0, 0},
+    //     {0, -1, 5, -1, 0},
+    //     {0, 0, -1, 0, 0},
+    //     {0, 0, 0, 0, 0}
+    // };
+
+
+    // DESENFOCADO 3x3
+    // vector<vector<int>> kernel = {
     //     {1, 1, 1},
     //     {1, 1, 1},
     //     {1, 1, 1}
     // };
 
-    int kernel[3][3] = {
-        {0, 0, 0},
-        {0, 1, 0},
-        {0, 0, 0}
+    // DESENFOCADO 5x5
+    vector<vector<int>> kernel = {
+        {0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 0},
+        {0, 1, 1, 1, 0},
+        {0, 1, 1, 1, 0},
+        {0, 0, 0, 0, 0}
     };
+
+
+    // IDENTIDAD
+    // vector<vector<int>> kernel = {
+    //     {0, 0, 0},
+    //     {0, 1, 0},
+    //     {0, 0, 0}
+    // };
 
     Mat image, newImage;
 
@@ -51,61 +84,50 @@ int main(int argc, char **argv)
     
     newImage = Mat(image.rows, image.cols, CV_8UC3, Scalar(0,0,0));
     
-    int kernel_size = 9;//kernel.size()() * kernel.size()[0];
-    for (size_t i = 0; i < image.rows; i++)
+    int kernel_total = 0;
+    for (int i = 0; i < kernel.size(); i++)
     {
-        for (size_t j = 0; j < image.cols; j++)
+        for (int j = 0; j < kernel.size(); j++)
+        {
+            kernel_total += kernel[i][j];
+        }
+    }   
+    int inicio = -(int)(kernel.size() / 2);
+    int final = (int)(kernel.size() / 2) + 1;
+    int offset = (int)(kernel.size() / 2);
+
+    cout<< inicio << ", " << final << endl;
+    for (int i = 0; i < image.rows; i++)
+    {
+        for (int j = 0; j < image.cols; j++)
         {
             int newPixel[] = {0,0,0};
+            
 
             // pixeles vecinos
-            for (int ki = -1; ki < 2; ki++)
+            for (int ki = inicio; ki < final; ki++)
             {
-                for (int kj = -1; kj < 2; kj++)
+                for (int kj = inicio; kj < final; kj++)
                 {
-
                     if( i + ki >= 0 && i + ki < image.rows && j + kj >= 0 && j + kj < image.cols ){
+                        
                         Vec3b pixel = image.at<Vec3b>(i + ki, j + kj);
 
                         int B = (int)pixel[0];
                         int G = (int)pixel[1];
                         int R = (int)pixel[2];
-                        newPixel[0] += (kernel[-ki + 1][-kj + 1] * B);
-                        newPixel[1] += (kernel[-ki + 1][-kj + 1] * G);
-                        newPixel[2] += (kernel[-ki + 1][-kj + 1] * R);
+
+                        newPixel[0] += (kernel[-ki + offset][-kj + offset] * B);
+                        newPixel[1] += (kernel[-ki + offset][-kj + offset] * G);
+                        newPixel[2] += (kernel[-ki + offset][-kj + offset] * R);
                     }
-
-
-
                 }
             }
 
-            // newPixel[0] /= kernel_size;
-            // newPixel[1] /= kernel_size;
-            // newPixel[2] /= kernel_size;
-            if (newPixel[0]<0)
-            {
-                newPixel[0]=0;
-            }
-            if (newPixel[1]<0)
-            {
-                newPixel[1]=0;
-            }
-            if (newPixel[2]<0)
-            {
-                newPixel[2]=0;
-            }
-            if (newPixel[0]>255)
-            {
-                newPixel[0]=255;
-            }
-            if (newPixel[1]>255)
-            {
-                newPixel[1]=255;
-            }
-            if (newPixel[2]>255)
-            {
-                newPixel[2]=255;
+            for (int k = 0; k < 3; k++){
+                newPixel[k] /= kernel_total;
+                newPixel[k]= max(newPixel[k], 0);
+                newPixel[k]= min(newPixel[k], 255);
             }
 
             newImage.at<Vec3b>(i,j)[0] = newPixel[0];
@@ -122,8 +144,8 @@ int main(int argc, char **argv)
     
 
     imshow("Display Filter2D", dst);
-    imshow("Original", image);                    // Show our image inside it.
-    imshow("Modificado", newImage);                    // Show our image inside it.
+    imshow("Original", image); // Show our image inside it.
+    imshow("Modificado", newImage);// Show our image inside it.
 
     waitKey(0); // Wait for a keystroke in the window
     return 0;
