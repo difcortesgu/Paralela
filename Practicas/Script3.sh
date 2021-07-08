02d01a0b4eb53434335b7508ec3df79b3339f0ac
jsalasm <jsalasm@unal.edu.co>
2021-07-08 13:49:27 -0500

    complementacion de archivos para la entrega practica 2 openmp

---
+++ Practicas/Script3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd 2/
+g++ image-effect.cpp -fopenmp -o image-effect `pkg-config --cflags --libs opencv`
+
+hilos[0]=1
+hilos[1]=2
+hilos[2]=4
+hilos[3]=8
+hilos[4]=16
+
+imagenes=('hd1' 'fullhd1' '4k1')
+for i in {0..2}; 
+do for k in {0..4}; 
+do for j in {1..10};
+do   ./image-effect imagenes/${imagenes[$i]}.jpg ${imagenes[$i]}_output.jpg ${hilos[$k]} 4 >> datosF.txt; 
+done ;
+done ;
+done
+
+imagenes=('hd1' 'fullhd1' '4k1')
+for i in {0..2}; 
+do for k in {0..4}; 
+do for j in {1..10};
+do   ./image-effect imagenes/${imagenes[$i]}.jpg ${imagenes[$i]}_output.jpg ${hilos[$k]} 7 >> datosF.txt; 
+done ;
+done ;
+done
